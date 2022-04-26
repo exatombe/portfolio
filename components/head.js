f6b478db91c25ba4997181657f3ad0f72bf79db8
garder500 <65552225+garder500@users.noreply.github.com>
2022-04-26 14:15:29 +0200

    Add files via upload

--- components/head.js
+++ components/head.js
@@ -8,20 +8,21 @@ export default function HeadComponent({ title, description}){
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <meta name="description" content={description} />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="application-name" content="Jeremy SOler" />
+        <meta name="application-name" content="Jeremy Soler" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Jeremy Soler" />
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
-        <meta property="og:site_name" content={title} />
+        <meta property="og:site_name" content="Jeremy Soler" />
         <meta name="keywords" content="Jeremy soler, web App, developper, mobile app, mobile developper, react native, react, nextjs, jeremysoler, jeremy soler, soler jeremy, jeremysoler.com, developpeur, mobile dev, react native dev, react dev, web app dev, dev web, developpeur react, developpeur PHP, developpeur Javascript, PHP, js, css, html, developpeur full stack, yaboku, bot-creator, The friend game, amis via app, making friend with yaboku" />
         <meta property="og:url" content="https://jeremysoler.com" />
-        <meta property="og:image" content="/favicon.com" />
+        <meta property="og:image" content="/favicon.ico" />
         <meta property="og:description" content={description}/>
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@garder500" />
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content= {description} />
+        <meta name="twitter:image" content="/favicon.ico" />
         <meta charset="utf-8" />
         <meta httpEquiv="Content-Type" content="text/html; charset=iso-8859-1" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=4" />

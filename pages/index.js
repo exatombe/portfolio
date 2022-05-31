f07e725f7fb2b2c0751fb940c89fdf65914eab0a
garder500 <jeremy27.clara22@gmail.com>
2022-05-31 17:34:24 +0200

    pushed my guy

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import { CssBaseline, Tabs, Tab, Box, Grid, Snackbar, Alert } from '@mui/materia
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import styles from '../styles/Home.module.css';
 import config from '../config.json';
 import fs from 'fs';
@@ -109,7 +111,7 @@ export default function Home(props) {
       })
       setOpen(true);
     }
-    if(state.errors.length > 0) {
+    if (state.errors.length > 0) {
       setAlertMsg({
         severity: 'error',
         msg: 'Une erreur est survenue, veuillez réessayer.',
@@ -118,84 +120,215 @@ export default function Home(props) {
     }
   }, [state.succeeded, state.errors]);
 
+
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <div className={styles.container} suppressHydrationWarning={true}>
         <HeadComponent title={config['root-data'].sitename} description={config["root-data"].sitedescription} />
-
+        <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={
+    JSON.parse(JSON.stringify({
+    "particles": {
+      "number": {
+        "value": 40,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.25,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 5,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.5,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "repulse"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 2
+        },
+        "repulse": {
+          "distance": 200
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true,
+    "config_demo": {
+      "hide_card": false,
+      "background_color": "#b61924",
+      "background_image": "",
+      "background_position": "50% 50%",
+      "background_repeat": "no-repeat",
+      "background_size": "cover"
+    }
+  }))
+}/>
         <main className={styles.main} >
-          <h1 className={styles.title}>
-            {config['root-data'].sitename}
-          </h1>
-          <span className={styles.subtitle}>{config['root-data'].description}</span>
-          <p className={styles.description}>
+        
+            <h1 className={styles.title}>
+              {config['root-data'].sitename}
+            </h1>
+            <span className={styles.subtitle}>{config['root-data'].description}</span>
+            <p className={styles.description}>
 
-          </p>
+            </p>
 
-          <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-              <Tabs value={value} onChange={handleChange} centered aria-label="basic tabs example">
-                <Tab label={"A propos"}    {...a11yProps(0)} />
-                <Tab label={"Articles"} {...a11yProps(1)} />
-                <Tab label={"Contact"} {...a11yProps(2)} />
-              </Tabs>
-            </Box>
-            <TabPanel value={value} index={0}>
-              <Markdown md={props.home} />
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-              <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-                {props.posts.map((post, index) => (
-                  <Grid key={index} xs={4} sm={4} md={4} item >
-                    <CardComponent title={post.title} description={post.description} image={post.image} link={post.link} />
-                  </Grid>
-                ))}
-              </Grid>
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
-              <Alert onClose={handleClose} severity={alertMsg.severity} sx={{ width: '100%' }}>
-                {alertMsg.msg}
-              </Alert>
-            </Snackbar>
-              <section className={styles.contact_form}>
-                <div className={styles.contact_box}>
-                  <div className={styles.contact_links}>
-                    <h2 className={styles.contact_form_title}>CONTACT</h2>
-                    <div className={styles.links}>
-                      <div className={styles.link}>
-                        <a href="https://www.linkedin.com/in/jeremy-soler-1b34b0200/" rel="noreferrer" target="_blank"><img className={styles.contact_form_img} src="https://i.postimg.cc/m2mg2Hjm/linkedin.png" alt="linkedin" /></a>
-                      </div>
-                      <div className={styles.link}>
-                        <a href="https://www.linkedin.com/in/jeremy-soler-1b34b0200/" rel="noreferrer" target="_blank"><img className={styles.contact_form_img} src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github" /></a>
-                      </div>
-                      <div className={styles.link}>
-                        <a href="mailto:contact@jeremysoler.com" ><img className={styles.contact_form_img} src="https://i.postimg.cc/NjLfyjPB/email.png" alt="email" /></a>
+            <Box sx={{ width: '100%' }}>
+              <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                <Tabs value={value} onChange={handleChange} centered aria-label="basic tabs example">
+                  <Tab label={"A propos"}    {...a11yProps(0)} />
+                  <Tab label={"Articles"} {...a11yProps(1)} />
+                  <Tab label={"Contact"} {...a11yProps(2)} />
+                </Tabs>
+              </Box>
+              <TabPanel value={value} index={0}>
+                <Markdown md={props.home} />
+              </TabPanel>
+              <TabPanel value={value} index={1}>
+                <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+                  {props.posts.map((post, index) => (
+                    <Grid key={index} xs={4} sm={4} md={4} item >
+                      <CardComponent title={post.title} description={post.description} image={post.image} link={post.link} />
+                    </Grid>
+                  ))}
+                </Grid>
+              </TabPanel>
+              <TabPanel value={value} index={2}>
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                  <Alert onClose={handleClose} severity={alertMsg.severity} sx={{ width: '100%' }}>
+                    {alertMsg.msg}
+                  </Alert>
+                </Snackbar>
+                <section className={styles.contact_form}>
+                  <div className={styles.contact_box}>
+                    <div className={styles.contact_links}>
+                      <h2 className={styles.contact_form_title}>CONTACT</h2>
+                      <div className={styles.links}>
+                        <div className={styles.link}>
+                          <a href="https://www.linkedin.com/in/jeremy-soler-1b34b0200/" rel="noreferrer" target="_blank"><img className={styles.contact_form_img} src="https://i.postimg.cc/m2mg2Hjm/linkedin.png" alt="linkedin" /></a>
+                        </div>
+                        <div className={styles.link}>
+                          <a href="https://github.com/garder500/" rel="noreferrer" target="_blank"><img className={styles.contact_form_img} src="https://i.postimg.cc/YCV2QBJg/github.png" alt="github" /></a>
+                        </div>
+                        <div className={styles.link}>
+                          <a href="mailto:contact@jeremysoler.com" ><img className={styles.contact_form_img} src="https://i.postimg.cc/NjLfyjPB/email.png" alt="email" /></a>
+                        </div>
                       </div>
                     </div>
+                    <div className={styles.contact_form_wrapper}>
+                      <form method="POST" onSubmit={handleSubmit}>
+                        <div className={styles.form_item}>
+                          <input className={styles.contact_form_input} type="text" name="sender" required />
+                          <label className={styles.contact_form_label}>Name:</label>
+                        </div>
+                        <div className={styles.form_item}>
+                          <input type="text" className={styles.contact_form_input} name="email" required />
+                          <label className={styles.contact_form_label}>Email:</label>
+                        </div>
+                        <div className={styles.form_item}>
+                          <textarea className={styles.contact_form_textarea} name="message" required></textarea>
+                          <label className={styles.contact_form_label}>Message:</label>
+                        </div>
+                        <button className={styles.submit_btn} type="submit">Send</button>
+                      </form>
+                    </div>
                   </div>
-                  <div className={styles.contact_form_wrapper}>
-                    <form method="POST" onSubmit={handleSubmit}>
-                      <div className={styles.form_item}>
-                        <input className={styles.contact_form_input} type="text" name="sender" required />
-                        <label className={styles.contact_form_label}>Name:</label>
-                      </div>
-                      <div className={styles.form_item}>
-                        <input type="text" className={styles.contact_form_input} name="email" required />
-                        <label className={styles.contact_form_label}>Email:</label>
-                      </div>
-                      <div className={styles.form_item}>
-                        <textarea className={styles.contact_form_textarea} name="message" required></textarea>
-                        <label className={styles.contact_form_label}>Message:</label>
-                      </div>
-                      <button className={styles.submit_btn} type="submit">Send</button>
-                    </form>
-                  </div>
-                </div>
-              </section>
-            </TabPanel>
-          </Box>
+                </section>
+              </TabPanel>
+            </Box>            
         </main>
 
         <footer className={styles.footer}>

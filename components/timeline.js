52bd9886766eb4330402827079d4b75b7431c5cf
garder500 <jeremy27.clara22@gmail.com>
2022-06-03 09:39:38 +0200

    Publish

--- components/timeline.js
+++ components/timeline.js
@@ -4,7 +4,6 @@ import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
 import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import SchoolIcon from '@mui/icons-material/School';
 import CorporateFareIcon from '@mui/icons-material/CorporateFare';
@@ -22,16 +21,23 @@ const bull = (
   </Box>
 );
 
-function BasicCard({ title, description, link = null }) {
+function BasicCard({ title, description, date, link = null }) {
   return (
     <div>
-         <Typography variant="h6" component="span">
+         <Typography sx={{
+            fontSize: '.9em'
+          }} component="span">
             {title}
           </Typography>
-          <Typography>{description}</Typography>
+          <Typography sx={{
+            fontSize: '0.7em'
+          }}>{description}</Typography>
       {link != null ? (
         <Button variant="contained" size="small" color="primary" href={link} rel="noreferrer" target="_blank">Voir plus</Button>
       ) : <></>}
+        <Typography sx={{
+            fontSize: '0.6em'
+          }} color="text.secondary">{date}</Typography>
     </div>
   );
 }
@@ -41,14 +47,6 @@ export default function CustomeTimeline() {
   return (
     <Timeline position="alternate">
       <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          align="right"
-          variant="body2"
-          color="text.secondary"
-        >
-          Juillet 2021
-        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot>
@@ -57,17 +55,10 @@ export default function CustomeTimeline() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-            <BasicCard title="BAC MELEC" description="Obtention du Bac MELEC" />
+            <BasicCard title="BAC MELEC" description="Obtention du Bac MELEC" date="Juillet 2021" />
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          variant="body2"
-          color="text.secondary"
-        >
-          Octobre 2021
-        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot color="primary">
@@ -76,17 +67,10 @@ export default function CustomeTimeline() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-            <BasicCard title="Entrée en BTS" description="Début de mon BTS"/>
+            <BasicCard title="Entrée en BTS" description="Début de mon BTS" date="Octobre 2021"/>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
-      <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          variant="body2"
-          color="text.secondary"
-        >
-          Novembre 2021
-        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot color="primary" variant="outlined">
@@ -95,17 +79,10 @@ export default function CustomeTimeline() {
           <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-            <BasicCard title="Entrée en alternance" description="Première expérience professionelle chez Aprentiv'"/>
+            <BasicCard title="Entrée en alternance" description="Première expérience professionelle chez Aprentiv'" date="Novembre 2021"/>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          variant="body2"
-          color="text.secondary"
-        >
-          Decembre 2021
-        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot color="primary">
@@ -114,17 +91,10 @@ export default function CustomeTimeline() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-            <BasicCard title="Veille technologique" description="Ecriture de ma veille techno"/>
+            <BasicCard title="Veille" description="Ecriture de ma veille technologique" date="Décembre 2021"/>
         </TimelineContent>
       </TimelineItem>
       <TimelineItem>
-        <TimelineOppositeContent
-          sx={{ m: 'auto 0' }}
-          variant="body2"
-          color="text.secondary"
-        >
-          Janvier 2022 - Juin 2022
-        </TimelineOppositeContent>
         <TimelineSeparator>
           <TimelineConnector />
           <TimelineDot color="primary">
@@ -133,7 +103,7 @@ export default function CustomeTimeline() {
           <TimelineConnector />
         </TimelineSeparator>
         <TimelineContent sx={{ py: '12px', px: 2 }}>
-            <BasicCard title="Début du PPE" description="Création d'un Site E-commerce" link="https://github.com/garder500/bts-site"/>
+            <BasicCard title="Début du PPE" description="Création d'un Site E-commerce" link="https://github.com/garder500/bts-site" date="Janvier 2022 - Juin 2022"/>
         </TimelineContent>
       </TimelineItem>
     </Timeline>
